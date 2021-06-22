f441c4a084e2bb7cc3c8c67f0e394b0f9c0c87b1
LT_Schmiddy (Alex Schmid) <schmidalex1996@gmail.com>
2021-06-22 13:35:10 -0400

    eliminated warnings.

--- src/main/java/net/lt_schmiddy/bettercrossbow/mixin/BetterQuickchargeMixin.java
+++ src/main/java/net/lt_schmiddy/bettercrossbow/mixin/BetterQuickchargeMixin.java
@@ -2,10 +2,6 @@ package net.lt_schmiddy.bettercrossbow.mixin;
 
 // import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-// import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
